18cf309d980eb845d2391df6040d2e4e2384e527
yedohkang913 <ykang@stuy.edu>
2017-12-06 18:25:39 -0500

    finished

--- sema.c
+++ sema.c
@@ -5,44 +5,46 @@
 #include <sys/sem.h>
 #include <string.h>
 
-#define KEY 123455
-
 int main(int argc, char* argv[]) {
-	/*int i = 0;
-	while(argv[i]){
-		printf("argv[%d]: %s\n", i, argv[i]);
-		i++;	
-	}*/
-	char * arg0;
-	char * arg1;
 	
-	if(argv[1]){
-		arg0 = argv[1];
-	}
-	else{
-		printf("Please input a command!\n");
-		exit(0);
-	}
-	if(argv[2]){
-		arg1 = argv[2];
+	int num = 0;
+	int ds = 0;
+	
+	int KEY = 12345;
+
+	if (argv[1] == NULL) {
+		printf("Please enter in an argument.");
+		exit(1);
 	}
 	
-	if (strcmp(arg0, "-c") == 0) {
+	if (strcmp(argv[1], "-c") == 0) {
+		ds = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0600);
+		num = atoi(argv[2]);
 		if(!argv[2]){
 			printf("-c requires a second command (integer)!\n");
 			exit(0);
 		}
-		
-		int ds = semget(KEY, 1, IPC_CREAT | IPC_EXCL);
-		if (ds != -1) {
-			printf("semaphore created: %d\n", ds);
-			printf("value set: 0\n");
-			semctl(ds, 0, SETVAL, arg1);
+		if (ds == -1) {
+			printf("semaphore already exists\n");
+			exit(1);
 		}
 		else {
-			printf("semaphore already exists\n");
+			printf("semaphore created: %d\n", ds);
+			printf("value set: %d\n", semctl(ds, 0, GETVAL));
+			semctl(ds, 0, SETVAL, num);
 		}
 	}
 	
+	if (strcmp(argv[1], "-v") == 0) {
+		ds = semget(KEY, 1, 0600);
+		printf("semaphore value: %d\n", semctl(ds, 0, GETVAL));
+	}
+	
+	if (strcmp(argv[1], "-r") == 0) {
+		ds = semget(KEY, 1, 0600);
+		printf("semaphore removed: %d\n", semctl(ds, 0, IPC_RMID));
+	}
+	
+	return 0;
 	
 }
